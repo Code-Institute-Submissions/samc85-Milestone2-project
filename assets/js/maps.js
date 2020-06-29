17d31422a5a9a5f189fd4ece90175cbabad27b41
Sam Croyle <sam300385@hotmail.com>
2020-06-29 19:46:16 +0000

    googlemaps api added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,41 +1,20 @@
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  var lakes = {lat:54.4609, lng: 3.0886};
+  // The map, centered at Uluru
 
 
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
 
+  var map2 = new.google.maps.Map(
+      document.getElementById('mapadventure'), {zoom: 4, center: lakes});
+  
 
+  var marker = new google.maps.Marker({position: uluru, map: map});
+  var marker2 = new google.maps.Marker({position: lakes, mapadventure: mapadventure});
 
+  }
 
-function initialize() {
-    var latlng = new google.maps.LatLng(18.520266,73.856406);
-    var latlng2 = new google.maps.LatLng(28.579943,77.330006);
-    var myOptions = {
-        zoom: 15,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var myOptions2 =  {
-        zoom: 15,
-        center: latlng2,
-        mapTypeId: google.maps.MapTypeId.SATELLITE
-    };
-
-    var map = new google.maps.Map(document.getElementById("maps"), myOptions);
-    
-    var map2 = new google.maps.Map(document.getElementById("mapadventure"), myOptions2);
-
-    var myMarker = new google.maps.Marker(
-    {
-        position: latlng,
-        map: map,
-        title:"Pune"
-   });
-
-    var myMarker2 = new google.maps.Marker(
-    {
-        position: latlng2,
-        map: map2,
-        title:"Noida"
-    });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
