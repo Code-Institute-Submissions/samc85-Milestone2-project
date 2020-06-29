eb31156f1b668fed0cca7736769fdf1ab918c34c
Sam Croyle <sam300385@hotmail.com>
2020-06-29 18:22:08 +0000

    google maps js code updated

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -7,16 +7,17 @@
 function initMap() {
   // The location of covent garden
   const coventGarden = {lat: 51.512945, lng: 0.125379};
-  // The map, centered at covent garden
-  const map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 8, center: coventGarden});
-  // The marker, positioned at covent garden
-  const marker = new google.maps.Marker({position: coventGarden, map: map});
 
- 
+
+  // The marker, positioned at covent garden
+const map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 8, center: coventGarden});
+
 const mapadventure = new google.maps.Map(
       document.getElementById('mapadventure'), {zoom: 8, center: coventGarden});
+
   // The marker, positioned at covent garden
-  const mark = new google.maps.Marker({position: coventGarden, mapadventure: mapadventure});
-}
+const marker = new google.maps.Marker({position: coventGarden, map: map});
+const mark = new google.maps.Marker({position: coventGarden, mapadventure: mapadventure});
+};
 
