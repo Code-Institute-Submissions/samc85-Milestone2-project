be8de3a4650061757ed357cf0e0ef45134fc5ca8
Sam Croyle <sam300385@hotmail.com>
2020-06-29 20:27:35 +0000

    function added for second google map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,20 +1,44 @@
-function initMap() {
+//function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  var lakes = {lat:54.4609, lng: 3.0886};
+  //var uluru = {lat: -25.344, lng: 131.036};
+  //var lakes = {lat:54.4609, lng: 3.0886};
   // The map, centered at Uluru
 
-
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
+  //var map = new google.maps.Map(
+    //  document.getElementById('map'), {zoom: 4, center: uluru});
   // The marker, positioned at Uluru
 
-  var map2 = new.google.maps.Map(
-      document.getElementById('mapadventure'), {zoom: 4, center: lakes});
+  //var map = new.google.maps.Map(
+    //  document.getElementById('mapadventure'), {zoom: 4, center: lakes});
   
 
-  var marker = new google.maps.Marker({position: uluru, map: map});
-  var marker2 = new google.maps.Marker({position: lakes, mapadventure: mapadventure});
+  //var marker = new google.maps.Marker({position: uluru, map: map});
+  //var marker2 = new google.maps.Marker({position: lakes, mapadventure: mapadventure});
 
-  }
+  //}
 
+
+function myMap() {
+var map = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+
+var map2 = {
+    center: new google.maps.LatLng(54.4609, 131.036),
+    zoom:5,
+};
+
+var map = new google.maps.Map(document.getElementById("map"),map);
+var map2 = new google.maps.Map(document.getElementById("mapadventure"),map2);
+};
+
+function myMap1() {
+
+var map2 = {
+    center: new google.maps.LatLng(54.4609, 131.036),
+    zoom:5,
+};
+
+var map2 = new google.maps.Map(document.getElementById("mapadventure"),map2);
+}
