674fa44f4c389c345d6c0a111631dfec60abde41
Sam Croyle <sam300385@hotmail.com>
2020-06-29 18:09:12 +0000

    adventure page text alignment

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,8 @@
 
 
+
+
+
 //map for index page contact us
 function initMap() {
   // The location of covent garden
@@ -9,18 +12,11 @@ function initMap() {
       document.getElementById('map'), {zoom: 8, center: coventGarden});
   // The marker, positioned at covent garden
   const marker = new google.maps.Marker({position: coventGarden, map: map});
-}
+
  
-
-
-//map for index page contact us
-function initMap() {
-  // The location of covent garden
-  const coventGarden = {lat: 51.512945, lng: 0.125379};
-  // The map, centered at covent garden
-  const map = new google.maps.Map(
+const mapadventure = new google.maps.Map(
       document.getElementById('mapadventure'), {zoom: 8, center: coventGarden});
   // The marker, positioned at covent garden
-  const marker = new google.maps.Marker({position: coventGarden, map: map});
+  const mark = new google.maps.Marker({position: coventGarden, mapadventure: mapadventure});
 }
 
