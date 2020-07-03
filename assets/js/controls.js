3b6250bb5836edd71cb50b09abab2af7263123ed
Sam Croyle <sam300385@hotmail.com>
2020-07-03 09:42:21 +0000

    jquery hide show buttons added to index page

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -4,6 +4,17 @@ $(document).ready(function(){
     $(".hidetext").toggle();
   });
 
+   $(".hide-comfort").click(function(){
+    $(".hidetext-comfort").toggle();
+  });
+
+  $(".hide-adventure").click(function(){
+    $(".hidetext-adventure").toggle();
+  });
+
+  $(".hide-country").click(function(){
+    $(".hidetext-country").toggle();
+  });
 
 
 });
