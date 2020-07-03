d16d9bc88ce5f980c2758c79659db7132a2ce397
Sam Croyle <sam300385@hotmail.com>
2020-07-03 09:57:40 +0000

    jquery toggle buttons added to adventure page

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -16,6 +16,29 @@ $(document).ready(function(){
     $(".hidetext-country").toggle();
   });
 
+  $(".hide-day1").click(function(){
+    $(".hidetext-day1").toggle();
+  });
+
+  $(".hide-day2").click(function(){
+    $(".hidetext-day2").toggle();
+  });
+
+  $(".hide-day3").click(function(){
+    $(".hidetext-day3").toggle();
+  });
+
+  $(".hide-day4").click(function(){
+    $(".hidetext-day4").toggle();
+  });
+
+  $(".hide-day5").click(function(){
+    $(".hidetext-day5").toggle();
+  });
+
+  $(".hide-day6").click(function(){
+    $(".hidetext-day6").toggle();
+  });
 
 });
 
