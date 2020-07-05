b3ba98f7f5beabc6f76b58b0a99b0d094cb04de6
Sam Croyle <sam300385@hotmail.com>
2020-07-05 08:07:06 +0000

    map center for keswick and bike lanes added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -5,8 +5,8 @@ function myMap1() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 54.6013,
+            lng: 3.1347
         },
         mapTypeControl: true,
         mapTypeControlOptions: {
@@ -24,32 +24,8 @@ function myMap1() {
         { lat: 40.754932, lng: -73.984016 }
     ];
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
-
-function myMap2() {
-    var map = new google.maps.Map(document.getElementById("mapcountry"), {
-        zoom: 6,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
-    });
-
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
+      var bikeLayer = new google.maps.BicyclingLayer();
+  bikeLayer.setMap(map);
 
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
