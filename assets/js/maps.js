ff230b215b15f1686062f88e262c7fa26be2fc45
Sam Croyle <sam300385@hotmail.com>
2020-07-05 08:16:32 +0000

    clusters added to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,38 +1,30 @@
-
-
-        
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 54.6013,
-            lng: 3.1347
-        },
-        mapTypeControl: true,
-        mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-        mapTypeIds: ["roadmap","terrain"]
-        }
-    });
 
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: {lat: -28.024, lng: 140.887}
+        });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
-
-      var bikeLayer = new google.maps.BicyclingLayer();
-  bikeLayer.setMap(map);
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
+          });
         });
-    });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = [
+        {lat: -31.563910, lng: 147.154312},
+        {lat: -33.718234, lng: 150.363181},
+        {lat: -33.727111, lng: 150.371124}
+      ]
