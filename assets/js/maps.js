f6c1a3abd35b621be6b759a0f410bd4b3d43ce7f
Sam Croyle <sam300385@hotmail.com>
2020-07-05 16:54:44 +0000

    grange location added to comfort google map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -87,3 +87,46 @@ function createMarker1(place) {
   });
 }
 
+
+
+/*-----   map for comfort page  ------*/
+
+
+function initMap2() {
+  let keswick = new google.maps.LatLng(54.547512, 3.158908);
+  infowindow = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById("mapcomfort"), {
+    center: keswick,
+    zoom: 10
+  });
+
+  let request = {
+    query: "Grange Lake District",
+    fields: ["name", "geometry"]
+  };
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker1(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
+}
+
+function createMarker2(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, "click", function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
