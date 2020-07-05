f2bdae10606cd5253d3ed9468cf9ac305ee059e5
Sam Croyle <sam300385@hotmail.com>
2020-07-05 14:15:51 +0000

    google maps location centered on keswick and places onclick feature added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,30 +1,99 @@
+
+/*---
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
+
+let map;
+let service;
+let infowindow;
+
+
+  let keswick = new google.maps.LatLng(54.6013, -3.1347);
+
+
+  infowindow = new google.maps.InfoWindow();
+
+  map = new google.maps.Map(
+      document.getElementById("map"), {center: keswick, zoom: 10});
+
+       let bikeLayer = new google.maps.BicyclingLayer();
+        bikeLayer.setMap(map);
+
+
+   let request = {
+    query: "Keswick",
+    fields: ["name", "geometry"],
+     };
+          exports.service = new google.maps.places.PlacesService(exports.map);
+          exports.service.findPlaceFromQuery(request, function(
+            results,
+            status
+          ) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+              for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+              }
+
+              exports.map.setCenter(results[0].geometry.location);
+            }
+          });
         }
-    });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+       function createMarker(place) {
+          let marker = new google.maps.Marker({
+            map: exports.map,
+            position: place.geometry.location
+          });
+          google.maps.event.addListener(marker, "click", function() {
+            exports.infowindow.setContent(place.name);
+            exports.infowindow.open(exports.map, this);
+          });
+        }
 
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
+        exports.createMarker = createMarker;
+        exports.initMap = initMap;
+      ((this.window = this.window || {}));
+---*/
+ 
+var map;
+var service;
+var infowindow;
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
+function initMap() {
+  var keswick = new google.maps.LatLng(54.6013, -3.1347);
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+  infowindow = new google.maps.InfoWindow();
 
-     var bikeLayer = new google.maps.BicyclingLayer();
-  bikeLayer.setMap(map);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: keswick,
+    zoom: 10
+  });
+
+  var request = {
+    query: "Lake District National Park",
+    fields: ["name", "geometry"]
+  };
+
+  service = new google.maps.places.PlacesService(map);
+
+  service.findPlaceFromQuery(request, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+
+      map.setCenter(results[0].geometry.location);
+    }
+  });
 }
 
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, "click", function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
