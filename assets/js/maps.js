55f03955451f07c596aab3c0647cf0fdb520a55d
Sam Croyle <sam300385@hotmail.com>
2020-07-11 12:35:16 +0000

    map markers with onlick feature added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,44 +2,66 @@
 
 
 
+
+
+
+
+// In the following example, markers appear when the user clicks on the map.
+// The markers are stored in an array.
+///The user can then click an option to hide, show or delete the markers.
+var map;
+var markers = [];
+
+
+
 function initMap() {
+  let Lakes = { lat: 54.4609, lng: -3.0886};
+  let Ambleside = { lat: 54.4287, lng: -2.9613};
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 3,
-          center: {lat: -28.024, lng: 140.887}
-        });
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: Lakes,
+    mapTypeId: "roadmap"
+  });
 
-        var map = new google.maps.Map(document.getElementById('adventure'), {
-          zoom: 8,
-          center: {lat: -28.024, lng: 140.887}
-        });
 
-    }
-        /*--
- var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // Adds a marker at the center of the map.
+  addMarker(Lakes);
+  addMarker(Ambleside);
+  
 
-        // Add some markers to the map.
-        // Note: The code uses the JavaScript Array.prototype.map() method to
-        // create an array of markers based on a given "locations" array.
-        // The map() method here has nothing to do with the Google Maps API.
-        var markers = locations.map(function(location, i) {
-          return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-          });
-        });
+}
 
-        // Add a marker clusterer to manage the markers.
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      }
-      var locations = [
-        {lat: -31.563910, lng: 147.154312},
-        {lat: -33.718234, lng: 150.363181},
-        {lat: -33.727111, lng: 150.371124},
-        {lat: -33.848588, lng: 151.209834},
-        {lat: -33.851702, lng: 151.216968},
-        {lat: -34.671264, lng: 150.863657}
-      ]
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+  let marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markers.push(marker);
+}
 
---*/
+// This event listener will call addMarker() when the map is clicked.
+  map.addListener("click", function(event) {
+    addMarker(event.latLng);
+  });
+
+
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
