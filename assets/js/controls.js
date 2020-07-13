64fd506e034b7e1201b28eb7e3233f8a00631ab9
Sam Croyle <sam300385@hotmail.com>
2020-07-13 19:13:16 +0000

    quiz template creater

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -31,12 +31,9 @@ $("#hide-comfort").click(function(){
   $("#comfort-package").show();
 });
 
-// Quiz
 
-// function to calculate the result of the survey
+//quiz
 
-
-   
 $("#explore").click(function(){
     $(".pre-result").hide();
 });
@@ -55,7 +52,126 @@ $("#explore").click(function(){
     alert("Time to explore");
 });
     
-//gallery images
+
+var myQuestions = [
+    {
+        question: "What would you rather?",
+        answers: {
+            a: 'Exploring somewhere new',
+            b: 'Comfort, comfort and more comfort',
+            c: 'Push myself to try new activities'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Choose the word that best suits you?",
+        answers: {
+            a: 'Thrill-seeker ',
+            b: 'Laid-back ',
+            c: 'Stressed '
+        },
+        correctAnswer: 'c'
+    },
+    {
+         question: "My dream holiday would be...",
+        answers: {
+            a: 'Relaxing',
+            b: 'Exploring',
+            c: 'A bit of everything'
+        },
+        correctAnswer: 'c'
+    }
+    ];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+
+            // for each available answer...
+            for(letter in questions[i].answers){
+
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type= "radio" name="question' +i+ '" value="' +letter+ '">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                        + '<div> </div>'
+                    + '</label>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+
+}
+
 
 
 
