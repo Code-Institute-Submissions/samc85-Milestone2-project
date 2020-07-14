d7ce2dc22ce9ba6607064349663419dd011bab2c
Sam Croyle <sam300385@hotmail.com>
2020-07-14 18:05:38 +0000

    quiz image gallery added jquery

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -52,6 +52,111 @@ $("#explore").click(function(){
     alert("Time to explore");
 });
     
+//gallery
+
+$(document).ready(function(){
+  $(".arnside").click(function(){
+    $(".arnside").hide("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".arnside-text").click(function(){
+    $(".arnside-text").hide("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".arnside-text").click(function(){
+    $(".arnside").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".arnside").click(function(){
+    $(".arnside-text").show("slow");
+  });
+});
+
+//------------
+
+$(document).ready(function(){
+  $(".bird").click(function(){
+    $(".bird").hide("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".bird-text").click(function(){
+    $(".bird-text").hide("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $(".bird-text").click(function(){
+    $(".bird").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".bird").click(function(){
+    $(".bird-text").show("slow");
+  });
+});
+
+//------
+
+$(document).ready(function(){
+  $(".bike").click(function(){
+    $(".bike").hide("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".bike-text").click(function(){
+    $(".bike-text").hide("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".bike-text").click(function(){
+    $(".bike").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".bike").click(function(){
+    $(".bike-text").show("slow");
+  });
+});
+
+//------
+
+$(document).ready(function(){
+  $(".beach").click(function(){
+    $(".beach").hide("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".beach-text").click(function(){
+    $(".beach-text").hide("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $(".beach-text").click(function(){
+    $(".beach").show("slow");
+  });
+});
+
+$(document).ready(function(){
+  $(".beach").click(function(){
+    $(".beach-text").show("slow");
+  });
+});
 
 //quiz
 
