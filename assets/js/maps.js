2567e9540f3dbdbc56c48cc799ad391a6461058f
Sam Croyle <sam300385@hotmail.com>
2020-07-17 14:04:45 +0000

    google maps reloaded onto discover page with market clusters ready to customize

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,68 +1,51 @@
-
-
-var adventure = [
-        {
-            name: "National Park",
-            lat: 54.4609, 
-            lng: -3.0886
-        },
-
-        {
-            name: "Ambleside",
-            lat: 54.4287,
-            lng: -2.9613
-        }
-];
-
-var markers = [];
-
-var London = {lat: 54.5409, lng: -3.0886};
-
-console.log(adventure);
-
 function initMap() {
 
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: London,
-    mapTypeId: "roadmap"
-  });
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: {lat: -28.024, lng: 140.887}
+        });
 
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-  // Adds a marker at the center of the map.
-  addMarker(location);
-  
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
 
-}
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
 
-// Adds a marker to the map and push to the array.
-function addMarker(adventure) {
-  let markers = new google.maps.Marker({
-    position: location,
-    map: map
-  });
-  markers.push(adventure);
-}
-
-// This event listener will call addMarker() when the map is clicked.
-  map.addListener("click", function(event) {
-    addMarker(event.latLng);
-  });
-
-
-// Sets the map on all markers in the array.
-function setMapOnAll(map) {
-  for (var i = 0; i < adventure.length; i++) {
-    adventure[i].setMap(map);
-  }
-}
-
-// Removes the markers from the map, but keeps them in the array.
-function clearMarkers() {
-  setMapOnAll(null);
-}
-
-// Shows any markers currently in the array.
-function showMarkers() {
-  setMapOnAll(map);
-};
+       var locations = [
+        {lat: -31.563910, lng: 147.154312},
+        {lat: -33.718234, lng: 150.363181},
+        {lat: -33.727111, lng: 150.371124},
+        {lat: -33.848588, lng: 151.209834},
+        {lat: -33.851702, lng: 151.216968},
+        {lat: -34.671264, lng: 150.863657},
+        {lat: -35.304724, lng: 148.662905},
+        {lat: -36.817685, lng: 175.699196},
+        {lat: -36.828611, lng: 175.790222},
+        {lat: -37.750000, lng: 145.116667},
+        {lat: -37.759859, lng: 145.128708},
+        {lat: -37.765015, lng: 145.133858},
+        {lat: -37.770104, lng: 145.143299},
+        {lat: -37.773700, lng: 145.145187},
+        {lat: -37.774785, lng: 145.137978},
+        {lat: -37.819616, lng: 144.968119},
+        {lat: -38.330766, lng: 144.695692},
+        {lat: -39.927193, lng: 175.053218},
+        {lat: -41.330162, lng: 174.865694},
+        {lat: -42.734358, lng: 147.439506},
+        {lat: -42.734358, lng: 147.501315},
+        {lat: -42.735258, lng: 147.438000},
+        {lat: -43.999792, lng: 170.463352}
+      ]
