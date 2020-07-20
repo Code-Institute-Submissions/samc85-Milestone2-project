07269d28ecff6e064a1196ca7d39740f4b065dc6
Sam Croyle <sam300385@hotmail.com>
2020-07-20 20:39:39 +0000

    map zoom increased

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
   
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 7,
         center: {
             lat: 54.4609,
             lng: -3.0886
