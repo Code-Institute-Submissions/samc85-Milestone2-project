a75b64ac348a2799a15724436976e069eb6acee3
Sam Croyle <sam300385@hotmail.com>
2020-07-20 20:33:54 +0000

    google map centering on lake district

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -3,15 +3,15 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 54.4609,
+            lng: -3.0886
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
+        { name: 'Town', lat: 40.785091, lng: -73.968285 },
         { lat: 41.084045, lng: -73.874245 },
         { lat: 40.754932, lng: -73.984016 }
     ];
