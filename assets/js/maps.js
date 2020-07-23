5e48f57b3c46c15cf6bdb55e1e51510760e4919b
Sam Croyle <sam300385@hotmail.com>
2020-07-23 15:04:52 +0000

    google map buttons added with links

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,90 +1,44 @@
   
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 9,
+    let map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
         center: {
             lat: 54.4609,
             lng: -3.0886
         }
     });
-
-
-
-        var locations = [
-      
-        {lat: 54.6013, lng: -3.1347},
-        {lat: 54.3739, lng: -2.9376},
-        {lat: 54.3758, lng: -2.9994},
-        {lat: 54.1993, lng: -2.9496},
-        {lat: -34.671264, lng: 150.863657},
-      ]
-
      
-      var Ambleside = new google.maps.Marker({position: {lat:54.4287, lng: -2.9613}});
-        Ambleside.setMap(map);
 
-      google.maps.event.addListener(Ambleside,'click',function() {
-        map.setZoom(15);
-        map.setCenter(Ambleside.getPosition());
-        });
-
-
-        // Add some markers to the map.
-        // Note: The code uses the JavaScript Array.prototype.map() method to
-        // create an array of markers based on a given "locations" array.
-        // The map() method here has nothing to do with the Google Maps API.
-        /*var markers = locations.map(function(location, i) {
-          return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-          });
-        });
-*/
-        // Add a marker clusterer to manage the markers.
-        /*(var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      
-*/
         var bikeLayer = new google.maps.BicyclingLayer();
         bikeLayer.setMap(map);
 
   
-  /* var request = {
-    placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-    fields: ["name", "formatted_address", "place_id", "geometry"]
-  };
-
-  var infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-
-  service.getDetails(request, function(place, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-      });
-      google.maps.event.addListener(marker, "click", function() {
-        infowindow.setContent(
-          "<div><strong>" +
-            place.name +
-            "</strong><br>" +
-            "Place ID: " +
-            place.place_id +
-            "<br>" +
-            place.formatted_address +
-            "</div>"
-        );
-        infowindow.open(map, this);
-      });
-    }
+   $(".towns").click(function() {
+  console.log(this.dataset.lat+","+this.dataset.lng);
+  map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
+  map.setZoom(14);
   });
-*/
+  const towns = [
+    ['ambleside', 54.4287, -2.9613],
+    ['keswick',  54.6013, -3.1347],
+    ['bowness-on-windermere', 54.3647, -2.9196],   
+    ['grasmere', 54.4597, -3.0244],  
+    ['hawkshead', 54.3758, -2.9994],
+    ['lake-district', 54.4609, -3.0886]
+  ]
+
+  let bounds = new google.maps.LatLngBounds();
+  for (i = 0; i < towns.length; i++) {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: towns[i][1],
+        lng: towns[i][2]
+      },
+      title: towns[i][0],
+      map: map
+    });
+    bounds.extend(marker.getPosition());
+  }
+  map.fitBounds(bounds);
 }
 
-$(document).ready(function() {
-    $("#maptest").on('click', function() {
-    map.setZoom(15);
-    map.setCenter(Ambleside.getPosition());
-   });
-})
-   
