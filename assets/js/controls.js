b71840153a227367fb0e8f3463a532ec754db516
Sam Croyle <sam300385@hotmail.com>
2020-07-24 07:22:03 +0000

    code through formatters for beautifying

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -1,287 +1,292 @@
-$(document).ready(function(){
-// Popover function 
-$(function () {
-  $("[data-toggle='popover']").popover({
-      html: true,
-  });
-});
-// Country gallery
-$(".bath").click(function(){
-    $(".bath-text").slideToggle("slow");
-});
-
-$(".country-day1and2").click(function(){
-    $(".bath").hide();
-    $(".beachkite").hide();
-    $(".cartmel").hide();
-    $(".dogchase").hide();
-    $(".bath-text").hide();
-    $(".cartmel-text").hide();
-    $(".dogchase-text").hide();
-});
-
-$(".country-day1and2").click(function(){
-    $(".beachkite").show("slow");
-    $(".beachkite-text").slideToggle("slow");
-});
-
-$(".country-day3and4").click(function(){
-    $(".beachkite").hide();
-    $(".bath").hide();
-    $(".dogchase").hide();
-    $(".bath-text").hide();
-    $(".beachkite-text").hide();
-    $(".dogchase-text").hide();
-});
-
-$(".country-day3and4").click(function(){
-    $(".cartmel").show("slow");
-    $(".cartmel-text").slideToggle("slow");
-});
-
-$(".country-day5and6").click(function(){
-    $(".cartmel").hide();
-    $(".beachkite").hide();
-    $(".bath").hide();
-    $(".bath-text").hide();
-    $(".cartmel-text").hide();
-    $(".beachkite-text").hide();
-});
-
-$(".country-day5and6").click(function(){
-    $(".dogchase").show("slow");
-    $(".dogchase-text").slideToggle("slow");
-});
-
-//---- adventure packages -----//
-
-$(".friends").click(function(){
-    $(".adventure-text").slideToggle("slow");
-});
-
-$(".adventure-day1and2").click(function(){
-    $(".friends").hide();
-    $(".sail").hide();
-    $(".hike").hide();
-    $(".adventure-3and4-text").hide();
-    $(".adventure-5and6-text").hide();
-    $(".adventure-text").hide();
-});
-
-$(".adventure-day1and2").click(function(){
-    $(".cycle-pair").show("slow");
-    $(".adventure-1and2-text").slideToggle("slow");
-});
-
-$(".adventure-day3and4").click(function(){
-    $(".cycle-pair").hide();
-    $(".friends").hide();
-    $(".sail").hide();
-    $(".adventure-1and2-text").hide();
-    $(".adventure-5and6-text").hide();
-    $(".adventure-text").hide();
-});
-
-$(".adventure-day3and4").click(function(){
-    $(".hike").show("slow");
-    $(".adventure-3and4-text").slideToggle("slow");
-});
-
-$(".adventure-day5and6").click(function(){
-    $(".hike").hide();
-    $(".cycle-pair").hide();
-    $(".friends").hide();
-    $(".adventure-1and2-text").hide();
-    $(".adventure-3and4-text").hide();
-    $(".adventure-text").hide();
-});
-
-$(".adventure-day5and6").click(function(){
-    $(".sail").show("slow");
-    $(".adventure-5and6-text").slideToggle("slow");
-});
-
-//--- comfort packages ----//
-
-$(".farm").click(function(){
-    $(".farm-text").slideToggle("slow");
-});
-
-$(".comfort-day1and2").click(function(){
-    $(".farm").hide();
-    $(".farm-text").hide();
-    $(".picnic").hide();
-    $(".picnic-text").hide();
-    $(".forest").hide();
-    $(".forest-text").hide();
-    $(".window").hide();
-    $(".window-text").hide();
-});
-
-$(".comfort-day1and2").click(function(){
-    $(".picnic").show("slow");
-    $(".picnic-text").slideToggle("slow");
-});
-
-$(".comfort-day3and4").click(function(){
-    $(".farm").hide();
-    $(".farm-text").hide();
-    $(".picnic").hide();
-    $(".picnic-text").hide();
-    $(".window").hide();
-    $(".window-text").hide();
-});
-
-$(".comfort-day3and4").click(function(){
-    $(".forest").show("slow");
-    $(".forest-text").slideToggle("slow");
-});
-
-$(".comfort-day5and6").click(function(){
-    $(".farm").hide();
-    $(".farm-text").hide();
-    $(".picnic").hide();
-    $(".picnic-text").hide();
-    $(".forest").hide();
-    $(".forest-text").hide();
-});
-
-$(".comfort-day5and6").click(function(){
-    $(".window").show("slow");
-    $(".window-text").slideToggle("slow");
-});
-
-//-----  Packages  --------//
-
-$("#show-adventure").click(function(){
-  $("#adventure-package").hide();
-});
-
-$("#hide-adventure").click(function(){
-  $("#adventure-package").show();
-});
-
-$("#show-country").click(function(){
-  $("#country-package").hide();
-});
-
-$("#hide-country").click(function(){
-  $("#country-package").show();
-});
-
-$("#show-comfort").click(function(){
-  $("#comfort-package").hide();
-});
-
-$("#hide-comfort").click(function(){
-  $("#comfort-package").show();
-});
-
-// fact cards
-$(document).ready(function(){
-    $(".fact2").click(function(){
-        $(".show-fact2").slideToggle("slow");
+$(document).ready(function () {
+    // Popover function
+    $(function () {
+        $("[data-toggle='popover']").popover({
+            html: true,
         });
-    $(".fact1").click(function(){
-        $(".show-fact1").slideToggle("slow");
-        });
-     $(".fact3").click(function(){
-        $(".show-fact3").slideToggle("slow");
     });
-});
+    // Country gallery
+    $(".bath").click(function () {
+        $(".bath-text").slideToggle("slow");
+    });
 
-//------- discover gallery
-$(document).ready(function(){
-  $(".countryroad").click(function(){
-    $(".countryroad-text").slideToggle("slow");
-  });
-});
+    $(".country-day1and2").click(function () {
+        $(".bath").hide();
+        $(".beachkite").hide();
+        $(".cartmel").hide();
+        $(".dogchase").hide();
+        $(".bath-text").hide();
+        $(".cartmel-text").hide();
+        $(".dogchase-text").hide();
+    });
 
-$(document).ready(function(){
-  $(".water").click(function(){
-    $(".water-text").slideToggle("slow");
-  });
-});
+    $(".country-day1and2").click(function () {
+        $(".beachkite").show("slow");
+        $(".beachkite-text").slideToggle("slow");
+    });
 
-$(document).ready(function(){
-  $(".wrestling").click(function(){
-    $(".wrestling-text").slideToggle("slow");
-  });
-});
+    $(".country-day3and4").click(function () {
+        $(".beachkite").hide();
+        $(".bath").hide();
+        $(".dogchase").hide();
+        $(".bath-text").hide();
+        $(".beachkite-text").hide();
+        $(".dogchase-text").hide();
+    });
 
-$(document).ready(function(){
-  $(".mine").click(function(){
-    $(".mine-text").slideToggle("slow");
-  });
-});
+    $(".country-day3and4").click(function () {
+        $(".cartmel").show("slow");
+        $(".cartmel-text").slideToggle("slow");
+    });
 
-$(document).ready(function(){
-  $(".fair").click(function(){
-    $(".fair-text").slideToggle("slow");
-  });
-});
+    $(".country-day5and6").click(function () {
+        $(".cartmel").hide();
+        $(".beachkite").hide();
+        $(".bath").hide();
+        $(".bath-text").hide();
+        $(".cartmel-text").hide();
+        $(".beachkite-text").hide();
+    });
 
-$(document).ready(function(){
-  $(".spa").click(function(){
-    $(".spa-text").slideToggle("slow");
-  });
-});
+    $(".country-day5and6").click(function () {
+        $(".dogchase").show("slow");
+        $(".dogchase-text").slideToggle("slow");
+    });
+
+    //---- adventure packages -----//
+
+    $(".friends").click(function () {
+        $(".adventure-text").slideToggle("slow");
+    });
+
+    $(".adventure-day1and2").click(function () {
+        $(".friends").hide();
+        $(".sail").hide();
+        $(".hike").hide();
+        $(".adventure-3and4-text").hide();
+        $(".adventure-5and6-text").hide();
+        $(".adventure-text").hide();
+    });
+
+    $(".adventure-day1and2").click(function () {
+        $(".cycle-pair").show("slow");
+        $(".adventure-1and2-text").slideToggle("slow");
+    });
+
+    $(".adventure-day3and4").click(function () {
+        $(".cycle-pair").hide();
+        $(".friends").hide();
+        $(".sail").hide();
+        $(".adventure-1and2-text").hide();
+        $(".adventure-5and6-text").hide();
+        $(".adventure-text").hide();
+    });
+
+    $(".adventure-day3and4").click(function () {
+        $(".hike").show("slow");
+        $(".adventure-3and4-text").slideToggle("slow");
+    });
+
+    $(".adventure-day5and6").click(function () {
+        $(".hike").hide();
+        $(".cycle-pair").hide();
+        $(".friends").hide();
+        $(".adventure-1and2-text").hide();
+        $(".adventure-3and4-text").hide();
+        $(".adventure-text").hide();
+    });
+
+    $(".adventure-day5and6").click(function () {
+        $(".sail").show("slow");
+        $(".adventure-5and6-text").slideToggle("slow");
+    });
+
+    //--- comfort packages ----//
+
+    $(".farm").click(function () {
+        $(".farm-text").slideToggle("slow");
+    });
+
+    $(".comfort-day1and2").click(function () {
+        $(".farm").hide();
+        $(".farm-text").hide();
+        $(".picnic").hide();
+        $(".picnic-text").hide();
+        $(".forest").hide();
+        $(".forest-text").hide();
+        $(".window").hide();
+        $(".window-text").hide();
+    });
+
+    $(".comfort-day1and2").click(function () {
+        $(".picnic").show("slow");
+        $(".picnic-text").slideToggle("slow");
+    });
+
+    $(".comfort-day3and4").click(function () {
+        $(".farm").hide();
+        $(".farm-text").hide();
+        $(".picnic").hide();
+        $(".picnic-text").hide();
+        $(".window").hide();
+        $(".window-text").hide();
+    });
+
+    $(".comfort-day3and4").click(function () {
+        $(".forest").show("slow");
+        $(".forest-text").slideToggle("slow");
+    });
+
+    $(".comfort-day5and6").click(function () {
+        $(".farm").hide();
+        $(".farm-text").hide();
+        $(".picnic").hide();
+        $(".picnic-text").hide();
+        $(".forest").hide();
+        $(".forest-text").hide();
+    });
+
+    $(".comfort-day5and6").click(function () {
+        $(".window").show("slow");
+        $(".window-text").slideToggle("slow");
+    });
+
+    //-----  Packages  --------//
+
+    $("#show-adventure").click(function () {
+        $("#adventure-package").hide();
+    });
+
+    $("#hide-adventure").click(function () {
+        $("#adventure-package").show();
+    });
+
+    $("#show-country").click(function () {
+        $("#country-package").hide();
+    });
+
+    $("#hide-country").click(function () {
+        $("#country-package").show();
+    });
+
+    $("#show-comfort").click(function () {
+        $("#comfort-package").hide();
+    });
+
+    $("#hide-comfort").click(function () {
+        $("#comfort-package").show();
+    });
+
+    // fact cards
+    $(document).ready(function () {
+        $(".fact2").click(function () {
+            $(".show-fact2").slideToggle("slow");
+        });
+        $(".fact1").click(function () {
+            $(".show-fact1").slideToggle("slow");
+        });
+        $(".fact3").click(function () {
+            $(".show-fact3").slideToggle("slow");
+        });
+    });
+
+    //------- discover gallery
+    $(document).ready(function () {
+        $(".countryroad").click(function () {
+            $(".countryroad-text").slideToggle("slow");
+        });
+    });
+
+    $(document).ready(function () {
+        $(".water").click(function () {
+            $(".water-text").slideToggle("slow");
+        });
+    });
+
+    $(document).ready(function () {
+        $(".wrestling").click(function () {
+            $(".wrestling-text").slideToggle("slow");
+        });
+    });
+
+    $(document).ready(function () {
+        $(".mine").click(function () {
+            $(".mine-text").slideToggle("slow");
+        });
+    });
+
+    $(document).ready(function () {
+        $(".fair").click(function () {
+            $(".fair-text").slideToggle("slow");
+        });
+    });
+
+    $(document).ready(function () {
+        $(".spa").click(function () {
+            $(".spa-text").slideToggle("slow");
+        });
+    });
 });
 
 //quiz
 
 // function to calculate the result of the survey
-$(document).ready(score=()=> {
-  // initialize variables for each choice's score
-  // If you add more choices and outcomes, you must add another variable here.
-  let c1score = 0;
-  let c2score = 0;
-  let c3score = 0;
-  let c4score = 0;
+$(document).ready(
+    (score = () => {
+        // initialize variables for each choice's score
+        // If you add more choices and outcomes, you must add another variable here.
+        let c1score = 0;
+        let c2score = 0;
+        let c3score = 0;
+        let c4score = 0;
 
-  // get a list of the radio inputs on the page
-  let choices = document.getElementsByTagName("input");
-  // loop through all the radio inputs
-  for (i=0; i<choices.length; i++) {
-    // if the radio is checked..
-    if (choices[i].checked) {
-      // add 1 to that choice's score
-      if (choices[i].value == "c1") {
-        c1score = c1score + 1;
-      }
-      if (choices[i].value == "c2") {
-        c2score = c2score + 1;
-      }
-      if (choices[i].value == "c3") {
-        c3score = c3score + 1;
-      }
-      if (choices[i].value == "c4") {
-        c4score = c4score + 1;
-      }
-      // If you add more choices and outcomes, you must add another if statement below.
-    }
-  }
-  // Find out which choice got the highest score.
-  // If you add more choices and outcomes, you must add the variable here.
-  let maxscore = Math.max(c1score,c2score,c3score,c4score);
-  // Display answer corresponding to that choice
-  let answerbox = document.getElementById("answer");
-  if (c1score == maxscore) { // If user chooses the first choice the most, this outcome will be displayed.
-    answerbox.innerHTML = "You are an adventure seeker... why not look at our Adventure package";
-  }
-  if (c2score == maxscore) { // If user chooses the second choice the most, this outcome will be displayed.
-    answerbox.innerHTML = "You appreciate the opportunity to relax and doing things at your leisure... why not look at our Comfort packages";
-  }
-  if (c3score == maxscore) { // If user chooses the third choice the most, this outcome will be displayed.
-    answerbox.innerHTML = "You seek the Country life... enjoying nature, local towns... why not explore our Country packages";
-  }  
-  // If you add more choices, you must add another response below.
-});
+        // get a list of the radio inputs on the page
+        let choices = document.getElementsByTagName("input");
+        // loop through all the radio inputs
+        for (i = 0; i < choices.length; i++) {
+            // if the radio is checked..
+            if (choices[i].checked) {
+                // add 1 to that choice's score
+                if (choices[i].value == "c1") {
+                    c1score = c1score + 1;
+                }
+                if (choices[i].value == "c2") {
+                    c2score = c2score + 1;
+                }
+                if (choices[i].value == "c3") {
+                    c3score = c3score + 1;
+                }
+                if (choices[i].value == "c4") {
+                    c4score = c4score + 1;
+                }
+                // If you add more choices and outcomes, you must add another if statement below.
+            }
+        }
+        // Find out which choice got the highest score.
+        // If you add more choices and outcomes, you must add the variable here.
+        let maxscore = Math.max(c1score, c2score, c3score, c4score);
+        // Display answer corresponding to that choice
+        let answerbox = document.getElementById("answer");
+        if (c1score == maxscore) {
+            // If user chooses the first choice the most, this outcome will be displayed.
+            answerbox.innerHTML = "You are an adventure seeker... why not look at our Adventure package";
+        }
+        if (c2score == maxscore) {
+            // If user chooses the second choice the most, this outcome will be displayed.
+            answerbox.innerHTML = "You appreciate the opportunity to relax and doing things at your leisure... why not look at our Comfort packages";
+        }
+        if (c3score == maxscore) {
+            // If user chooses the third choice the most, this outcome will be displayed.
+            answerbox.innerHTML = "You seek the Country life... enjoying nature, local towns... why not explore our Country packages";
+        }
+        // If you add more choices, you must add another response below.
+    })
+);
 
 // program the reset button
-$(document).ready(resetAnswer= ()=> {
-  let answerbox = document.getElementById("answer");
-  answerbox.innerHTML = "Your result will show up here!";
-});
-
-
+$(document).ready(
+    (resetAnswer = () => {
+        let answerbox = document.getElementById("answer");
+        answerbox.innerHTML = "Your result will show up here!";
+    })
+);
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,41 +1,40 @@
-  function initMap() {
+  function loadMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
         zoom: 2,
         center: {
             lat: 54.4609,
-            lng: -3.0886
-        }
+            lng: -3.0886,
+        },
     });
-     
+
     let bikeLayer = new google.maps.BicyclingLayer();
     bikeLayer.setMap(map);
 
-   $(".towns").click(function() {
-  console.log(this.dataset.lat+","+this.dataset.lng);
-  map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
-  map.setZoom(15);
-  });
-  const towns = [
-    ["ambleside", 54.4287, -2.9613],
-    ["keswick",  54.6013, -3.1347],
-    ["bowness-on-windermere", 54.3647, -2.9196],   
-    ["grasmere", 54.4597, -3.0244],  
-    ["hawkshead", 54.3758, -2.9994],
-    ["lake-district", 54.4609, -3.0886]
-  ];
-
-  let bounds = new google.maps.LatLngBounds();
-  for (i = 0; i < towns.length; i++) {
-    var marker = new google.maps.Marker({
-      position: {
-        lat: towns[i][1],
-        lng: towns[i][2]
-      },
-      title: towns[i][0],
-      map: map
+    $(".towns").click(function () {
+        console.log(this.dataset.lat + "," + this.dataset.lng);
+        map.setCenter(new google.maps.LatLng(this.dataset.lat, this.dataset.lng));
+        map.setZoom(15);
     });
-    bounds.extend(marker.getPosition());
-  }
-  map.fitBounds(bounds);
-}
+    const towns = [
+        ["ambleside", 54.4287, -2.9613],
+        ["keswick", 54.6013, -3.1347],
+        ["bowness-on-windermere", 54.3647, -2.9196],
+        ["grasmere", 54.4597, -3.0244],
+        ["hawkshead", 54.3758, -2.9994],
+        ["lake-district", 54.4609, -3.0886],
+    ];
 
+    let bounds = new google.maps.LatLngBounds();
+    for (i = 0; i < towns.length; i++) {
+        var marker = new google.maps.Marker({
+            position: {
+                lat: towns[i][1],
+                lng: towns[i][2],
+            },
+            title: towns[i][0],
+            map: map,
+        });
+        bounds.extend(marker.getPosition());
+    }
+    map.fitBounds(bounds);
+}
